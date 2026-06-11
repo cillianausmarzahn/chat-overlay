3deca79f2eafe140a3439926be9a003403eb7778
cillianausmarzahn <developercillo@gmail.com>
2026-06-11 02:49:02 +0200

    Update colon-fix.js

--- colon-fix.js
+++ colon-fix.js
@@ -4,10 +4,11 @@
     body { background: transparent !important; overflow: hidden; margin: 0; }
     .highlight-chat { background: transparent !important; box-shadow: none !important; border: none !important; padding: 1px 4px !important; margin: 0 !important; }
     .highlight-chat .hl-message { background: transparent !important; box-shadow: none !important; display: flex !important; flex-wrap: wrap !important; align-items: center !important; font-family: 'Open Sans', sans-serif !important; }
-    .highlight-chat .hl-message .hl-badges img { height: 20px !important; max-height: 20px !important; width: auto !important; vertical-align: middle !important; margin-right: 2px !important; }
-    .highlight-chat .hl-message .hl-name { font-family: 'Open Sans', sans-serif !important; font-size: 17px !important; font-weight: 700 !important; background: transparent !important; padding: 0 !important; margin-right: 4px !important; text-shadow: 1px 1px 2px rgba(0,0,0,.9), -1px -1px 2px rgba(0,0,0,.9) !important; }
-    .highlight-chat .hl-message .hl-content { color: #fff !important; -webkit-text-fill-color: #fff !important; font-family: 'Open Sans', sans-serif !important; font-size: 17px !important; font-weight: 700 !important; text-shadow: 1px 1px 2px rgba(0,0,0,.9), -1px -1px 2px rgba(0,0,0,.9) !important; }
-    .highlight-chat .hl-message .hl-content img { height: 32px !important; width: auto !important; vertical-align: middle !important; }
+    .highlight-chat .hl-message .hl-badges { margin-right: 2px !important; }
+    .highlight-chat .hl-message .hl-badges img { height: 15px !important; max-height: 15px !important; width: auto !important; vertical-align: middle !important; margin-right: 1px !important; }
+    .highlight-chat .hl-message .hl-name { font-family: 'Open Sans', sans-serif !important; font-size: 14px !important; font-weight: 800 !important; background: transparent !important; padding: 0 !important; margin-left: 0 !important; margin-right: 4px !important; text-shadow: 1px 1px 2px rgba(0,0,0,.9), -1px -1px 2px rgba(0,0,0,.9) !important; }
+    .highlight-chat .hl-message .hl-content { color: #fff !important; -webkit-text-fill-color: #fff !important; font-family: 'Open Sans', sans-serif !important; font-size: 14px !important; font-weight: 800 !important; text-shadow: 1px 1px 2px rgba(0,0,0,.9), -1px -1px 2px rgba(0,0,0,.9) !important; }
+    .highlight-chat .hl-message .hl-content img { height: 26px !important; width: auto !important; vertical-align: middle !important; }
   `;
   var style = document.createElement('style');
   style.textContent = css;
